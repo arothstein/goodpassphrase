3f7932919c50a678ebfab1714c21c1fe4c661f97
Aaron Rothstein <aaron.rothstein@wunita.com>
2020-06-10 07:16:22 -0500

    Removed/changed template content.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,32 +11,14 @@
           @click="leftDrawerOpen = !leftDrawerOpen"
         />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title>GoodPassphrase.com</q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1">
       <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item-label header class="text-grey-8">Essential Links</q-item-label>
+        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
 
@@ -47,63 +29,63 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink'
+import EssentialLink from "components/EssentialLink";
 
 export default {
-  name: 'MainLayout',
+  name: "MainLayout",
 
   components: {
     EssentialLink
   },
 
-  data () {
+  data() {
     return {
       leftDrawerOpen: false,
       essentialLinks: [
         {
-          title: 'Docs',
-          caption: 'quasar.dev',
-          icon: 'school',
-          link: 'https://quasar.dev'
+          title: "Docs",
+          caption: "quasar.dev",
+          icon: "school",
+          link: "https://quasar.dev"
         },
         {
-          title: 'Github',
-          caption: 'github.com/quasarframework',
-          icon: 'code',
-          link: 'https://github.com/quasarframework'
+          title: "Github",
+          caption: "github.com/quasarframework",
+          icon: "code",
+          link: "https://github.com/quasarframework"
         },
         {
-          title: 'Discord Chat Channel',
-          caption: 'chat.quasar.dev',
-          icon: 'chat',
-          link: 'https://chat.quasar.dev'
+          title: "Discord Chat Channel",
+          caption: "chat.quasar.dev",
+          icon: "chat",
+          link: "https://chat.quasar.dev"
         },
         {
-          title: 'Forum',
-          caption: 'forum.quasar.dev',
-          icon: 'record_voice_over',
-          link: 'https://forum.quasar.dev'
+          title: "Forum",
+          caption: "forum.quasar.dev",
+          icon: "record_voice_over",
+          link: "https://forum.quasar.dev"
         },
         {
-          title: 'Twitter',
-          caption: '@quasarframework',
-          icon: 'rss_feed',
-          link: 'https://twitter.quasar.dev'
+          title: "Twitter",
+          caption: "@quasarframework",
+          icon: "rss_feed",
+          link: "https://twitter.quasar.dev"
         },
         {
-          title: 'Facebook',
-          caption: '@QuasarFramework',
-          icon: 'public',
-          link: 'https://facebook.quasar.dev'
+          title: "Facebook",
+          caption: "@QuasarFramework",
+          icon: "public",
+          link: "https://facebook.quasar.dev"
         },
         {
-          title: 'Quasar Awesome',
-          caption: 'Community Quasar projects',
-          icon: 'favorite',
-          link: 'https://awesome.quasar.dev'
+          title: "Quasar Awesome",
+          caption: "Community Quasar projects",
+          icon: "favorite",
+          link: "https://awesome.quasar.dev"
         }
       ]
-    }
+    };
   }
-}
+};
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,9 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
-  </q-page>
+  <q-page class="flex flex-center"></q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex"
+};
 </script>
