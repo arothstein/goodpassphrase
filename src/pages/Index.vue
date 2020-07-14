a08d938b79f1970110b5d737e2c849d4ede4ecc9
Aaron Rothstein <aaron.rothstein@wunita.com>
2020-07-14 00:05:23 -0500

    Commented future content add.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,6 +9,16 @@
         </div>
       </div>
       <PassphraseGenerator></PassphraseGenerator>
+<!--      <div class="row">
+        <div class="col-12">
+          <h2 class="text-h4">Additional Security Tips</h2>
+          <p>
+            <ul>
+              <li>Enable Multi-Factor Authentication (MFA) where you can. Most online accounts support it one way or another: hardware key, mobile app, or SMS text.</li>
+            </ul>
+          </p>
+        </div>
+      </div> -->
     </div>
   </q-page>
 </template>
