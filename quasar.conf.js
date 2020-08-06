3173179d55b81367f75290d031c6371aab6ec99a
Aaron Rothstein <aaron.rothstein@wunita.com>
2020-08-06 10:13:14 -0500

    quasar/app v2 upgrade

--- quasar.conf.js
+++ quasar.conf.js
@@ -11,9 +11,6 @@ const paths = [{ path: "/" }, { path: "/about" }, { path: "/privacy" }];
 
 module.exports = function(/* ctx */) {
   return {
-    // https://quasar.dev/quasar-cli/cli-documentation/supporting-ie
-    supportIE: false,
-
     // https://quasar.dev/quasar-cli/cli-documentation/supporting-ts
     supportTS: false,
 
@@ -51,7 +48,6 @@ module.exports = function(/* ctx */) {
       // Does not applies to modern builds.
       // transpileDependencies: [],
 
-      // modern: true, // https://quasar.dev/quasar-cli/modern-build
       // rtl: false, // https://quasar.dev/options/rtl-support
       // preloadChunks: true,
       // showProgress: false,
@@ -99,7 +95,7 @@ module.exports = function(/* ctx */) {
       //            (fastest compile time; minimum bundle size; most tedious)
       // * true   - Import everything from Quasar
       //            (not treeshaking Quasar; biggest bundle size; convenient)
-      all: "auto",
+      importStrategy: "auto",
 
       components: [],
       directives: [],
@@ -131,27 +127,27 @@ module.exports = function(/* ctx */) {
         theme_color: "#027be3",
         icons: [
           {
-            src: "statics/icons/icon-128x128.png",
+            src: "icons/icon-128x128.png",
             sizes: "128x128",
             type: "image/png"
           },
           {
-            src: "statics/icons/icon-192x192.png",
+            src: "icons/icon-192x192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "statics/icons/icon-256x256.png",
+            src: "icons/icon-256x256.png",
             sizes: "256x256",
             type: "image/png"
           },
           {
-            src: "statics/icons/icon-384x384.png",
+            src: "icons/icon-384x384.png",
             sizes: "384x384",
             type: "image/png"
           },
           {
-            src: "statics/icons/icon-512x512.png",
+            src: "icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png"
           }
