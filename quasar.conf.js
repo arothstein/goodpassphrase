1f1bf698fbc790dfbe12e0eaab19e726610044fb
Aaron Rothstein <aaron.rothstein@wunita.com>
2020-08-06 09:21:06 -0500

    Remove '#' from URLs

--- quasar.conf.js
+++ quasar.conf.js
@@ -44,7 +44,7 @@ module.exports = function(/* ctx */) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: "hash", // available values: 'hash', 'history'
+      vueRouterMode: "history", // available values: 'hash', 'history'
 
       // Add dependencies for transpiling with Babel (Array of regexes)
       // (from node_modules, which are by default not transpiled).
